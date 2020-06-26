a31de5d7b030e4ec903ad99b51a8b762473a6381
Omar David <oder1011119504@gmail.com>
2020-06-25 22:00:13 -0400

    add funcion comprobar_sintomas

--- Funciones.sql
+++ Funciones.sql
@@ -4,3 +4,17 @@ begin
     return (round(((sysdate - fecha_nac)/365),0));
 end;
 /
+create or replace function comprobar_sintoma (persona per_sin.pasaporte_persona_ps%type, sintoma per_sin.id_sintoma_ps%type) return boolean is
+CURSOR sintoma_persona IS select * from per_sin where pasaporte_persona_ps = persona and id_sintoma_ps = sintoma;
+registro per_sin%rowtype;
+presencia boolean := false;
+begin
+    open sintoma_persona;
+    fetch sintoma_persona into registro;
+    if (sintoma_persona%found) then
+        presencia := true;
+        fetch sintoma_persona into registro;
+    end if;
+    close sintoma_persona;
+    return presencia;
+end;
