69c34c0f11b14f2a77e14baf32945558e3dd0f21
Omar David <oder1011119504@gmail.com>
2020-07-17 18:39:55 -0400

    add reporte 7

--- COVID.sql
+++ COVID.sql
@@ -26,76 +26,115 @@ from lugar l, lugar l2, (select count(*)"cantidad" from persona p where
     p.status_persona = 'Infectado')x
 where l.identificacion_lugar.nombre = 'Brasil' and l.id_lugar_lugar = l2.id_lugar;
  
--- (select count(*)"cantidad" from persona p where 
-  --  p.fechadef_persona <> null)y;
-
 --REPORTE5
 select l.identificacion_lugar.imagen"Pais", la.fechai_la"Fecha de Inicio", la.fechaf_la"Fecha de fin", a.nombre_aislamiento"Modelo aplicado"
 from lugar l, lu_ais la, aislamiento a
 where la.id_lugar_la = l.id_lugar and la.id_aislamiento_la = a.id_aislamiento and l.identificacion_lugar.nombre = 'España' and a.tipo_aislamiento = 4;
 
-
---Procedimientos
-set serveroutput on; 
-
-create or replace procedure personasInfectada (pais lugar.identificacion_lugar.nombre%type, estado lugar.identificacion_lugar.nombre%type, edad number,
-patolgoia patologia.nombre_patologia%type) 
-is 
+--REPORTE 7
+create or replace procedure VUELOS_PASAJEROS (pais lugar.identificacion_lugar.nombre%type, fechai date, fechaf date, ret_cursor OUT sys_refcursor) is
 begin
-    declare 
-        CURSOR RECORRIDO IS select p.foto_persona"Foto", p.nombre1_persona"Primer nombre", p.nombre2_persona"Segundo nombre", p.apellido1_persona"Primer apellido", 
-            p.apellido2_persona "Segundo apellido", to_char(p.fechanac_persona, 'DD/MM/YYYY') "Fecha de nacimiento", l3.identificacion_lugar.imagen "País", 
-            p.genero_persona "Género", l2.identificacion_lugar.nombre "Estado", pt.nombre_patologia"Patologia"
-        from persona p, lugar l, lugar l2, lugar l3, per_pat pp, patologia pt
-        where p.id_lugar_persona = l.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar
-            and pp.pasaporte_persona_pp = p.pasaporte_persona and pp.id_patologia_pp = pt.id_patologia and l3.identificacion_lugar.nombre = pais
-            and l2.identificacion_lugar.nombre = estado and round(((TO_DATE(sysdate,'dd/mm/yyyy') - to_date(p.fechanac_persona,'dd/mm/yyyy'))/365),0) > edad 
-            and pt.nombre_patologia = patolgoia
-        order by p.fechanac_persona;
-        REGISTRO_TABLA RECORRIDO%ROWTYPE;
-    begin
-        OPEN RECORRIDO;
-        fetch recorrido into registro_tabla;
-            WHILE RECORRIDO%FOUND LOOP
-                DBMS_OUTPUT.put_line ('REGISTRO: '||);
-                fetch recorrido into registro_tabla;
-            END LOOP;
-        CLOSE RECORRIDO;
-    end;--declare
+    if(pais is null and fechai is null and fechaf is null) then
+        --NINGUN PARAMETROS
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar
+        order by v.fechai_vuelo;
+    elsif (pais is NULL) then 
+        --SIN PAÍS
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar and v.fechai_vuelo >= TO_DATE( '15/12/2019', 'DD/MM/YYYY HH24:MI:SS') and 
+            v.fechaf_vuelo <= TO_DATE( '15/02/2020', 'DD/MM/YYYY HH24:MI:SS')
+        order by v.fechai_vuelo;
+    elsif (pais is null and fechai is null) then
+        --SIN FechaI y SIN Pais
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar and v.fechaf_vuelo <= TO_DATE( '15/02/2020', 'DD/MM/YYYY HH24:MI:SS')
+        order by v.fechai_vuelo;
+    elsif (pais is null and fechaf is null) then
+        --SIN FechaF y SIN Pais
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar and v.fechai_vuelo >= TO_DATE( '15/12/2019', 'DD/MM/YYYY HH24:MI:SS')
+        order by v.fechai_vuelo;
+    elsif (fechai is null and fechaf is null) then
+        --Por Pais
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar and l3.identificacion_lugar.nombre = 'Venezuela'
+        order by v.fechai_vuelo;
+    elsif (fechaf is null) then
+        --Por Pais sin FechF
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar and l3.identificacion_lugar.nombre = 'Venezuela' and v.fechai_vuelo >= TO_DATE( '15/12/2019', 'DD/MM/YYYY HH24:MI:SS') 
+        order by v.fechai_vuelo;
+    elsif (fechai is null) then
+        --Por Pais sin FechaI
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar and l3.identificacion_lugar.nombre = 'Venezuela' and v.fechaf_vuelo <= TO_DATE( '15/02/2020', 'DD/MM/YYYY HH24:MI:SS')
+        order by v.fechai_vuelo;
+    elsif (pais is not null and fechai is not null and fechaf is not null) then
+        --TODOS LOS PARAMETROS
+        OPEN ret_cursor FOR
+        select v.identificacion_aerolinea.imagen"Aerolinea", pv.numero_vuelo_pv"N° Vuelo", l3.identificacion_lugar.nombre"Desde", l6.identificacion_lugar.nombre"Hasta",
+            to_char(v.fechai_vuelo,'DD/MM/YYYY')"Fecha de Inicio", to_char(v.fechaf_vuelo,'DD/MM/YYYY')"Fecha de Fin", p.pasaporte_persona"N° ID pasajero", 
+            p.foto_persona"Foto", p.nombre1_persona"Primer Nombre", COALESCE(p.nombre2_persona, ' ')"Segundo Nombre", p.apellido1_persona"Primer Apellido", 
+            p.apellido2_persona"Segundo Apellido"
+        from persona p, per_vue pv, vuelo v, lugar l, lugar l2, lugar l3, lugar l4, lugar l5, lugar l6
+        where p.pasaporte_persona = pv.pasaporte_persona_pv and pv.numero_vuelo_pv = v.numero_vuelo and pv.id_lugar_salida_vuelo_pv = l.id_lugar and 
+            pv.id_lugar_llegada_vuelo_pv = l4.id_lugar and l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l4.id_lugar_lugar = l5.id_lugar and 
+            l5.id_lugar_lugar = l6.id_lugar and l3.identificacion_lugar.nombre = 'Venezuela' and v.fechai_vuelo >= TO_DATE( '15/12/2019', 'DD/MM/YYYY HH24:MI:SS') and 
+            v.fechaf_vuelo <= TO_DATE( '15/02/2020', 'DD/MM/YYYY HH24:MI:SS')
+        order by v.fechai_vuelo;
+    end if;
 end;
-/
-create or replace procedure PruebapersonasInfectada is 
-begin
-    declare 
-        CURSOR RECORRIDO IS select p.nombre1_persona "nombre1_persona", p.pasaporte_persona"pasaporte_persona" from persona where pasaporte_persona > 730;
-        REGISTRO_TABLA recorrido%ROWTYPE;
-    begin
-        OPEN RECORRIDO;
-        fetch recorrido into registro_tabla;
-            WHILE RECORRIDO%FOUND LOOP
-                DBMS_OUTPUT.put_line ('REGISTRO: '||REGISTRO_TABLA.nombre1_persona || ', '|| REGISTRO_TABLA.pasaporte_persona);
-                fetch recorrido into registro_tabla;
-            END LOOP;
-        CLOSE RECORRIDO;
-    end;--declare
-end;
-/
-execute PruebapersonasInfectada;
 
 --pruebas
-SELECT
-    *
-FROM aislamiento;
-
-select l.id_lugar, l2.identificacion_lugar.nombre "Estado", l3.identificacion_lugar.nombre "Pais", l3.identificacion_lugar.imagen "Foto"
-from lugar l, lugar l2, lugar l3
-where l.id_lugar_lugar = l2.id_lugar and l2.id_lugar_lugar = l3.id_lugar and l3.identificacion_lugar.nombre = 'Brasil';
-
-select p.pasaporte_persona, p.nombre1_persona, pt.nombre_patologia
-from persona p, patologia pt, per_pat pp
-where p.pasaporte_persona = pp.pasaporte_persona_pp and pp.id_patologia_pp = pt.id_patologia;
-
-select p.pasaporte_persona, p.nombre1_persona, to_char(p.fechanac_persona, 'DD/MM/YYYY') "fechaNac"
-from persona p 
-where p.pasaporte_persona <= 10 and edad(p.fechanac_persona) < 21
-order by p.fechanac_persona;
+select * from per_vue;
+select * from vuelo;
